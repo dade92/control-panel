a8b91fb78be19f85d18e3c88cba7656aa079fd7d
Davide Botti <davidebotti92@gmail.com>
2024-01-20 13:12:41 +0100

    Review the API contract

--- webapp/src/main/frontend/server/Server.ts
+++ webapp/src/main/frontend/server/Server.ts
@@ -24,11 +24,15 @@ const thingsResponse = {
 };
 
 const things200 = (): Response => new Response(200, {}, thingsResponse);
+const things500 = (): Response => new Response(500);
+const switchStatus200 = (): Response => new Response(204);
+const switchStatus500 = (): Response => new Response(500);
 
 export const server: () => Server = () =>
     createServer({
         logging: true,
         routes() {
             this.get('/api/v1/things', things200, {timing: 1000});
+            this.post('/api/v1/switch/:deviceId/:thingId', switchStatus200, {timing: 1000});
         },
     });
