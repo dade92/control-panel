6149fd875d99605fd009ba424e13d481fda12a5d
Davide Botti <davidebotti92@gmail.com>
2024-01-21 17:06:19 +0100

    Fix

--- webapp/src/main/frontend/App.tsx
+++ webapp/src/main/frontend/App.tsx
@@ -3,6 +3,7 @@ import {server} from "./server/Server";
 import {ControlPanel} from "./ControlPanel";
 import {Typography} from "@mui/material";
 import styled from "styled-components";
+import {RestRetrieveThingsProvider} from "./RetrieveThingsProvider";
 
 if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_STAGE === 'dev') {
     console.log('Local dev mode detected, starting mirage server...');
@@ -18,6 +19,6 @@ const Wrapper = styled.div`
 export const App: FC = () => {
 
     return <Wrapper>
-        <ControlPanel/>
+        <ControlPanel retrieveThingsProvider={RestRetrieveThingsProvider}/>
     </Wrapper>
 }
--- webapp/src/main/frontend/RetrieveThingsProvider.ts
+++ webapp/src/main/frontend/RetrieveThingsProvider.ts
@@ -3,9 +3,8 @@ import {staticRestClient} from "./logic/RestClient";
 
 export type RetrieveThingsProvider = () => Promise<ApiResponse>;
 
-export const RestRetrieveThingsProvider: RetrieveThingsProvider = () => {
-    return staticRestClient.get<ApiResponse>('/v1/things');
-}
+export const RestRetrieveThingsProvider: RetrieveThingsProvider = () =>
+    staticRestClient.get<ApiResponse>('/v1/things')
 
 export interface ApiResponse {
     things: Thing[];
