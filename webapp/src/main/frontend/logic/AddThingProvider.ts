41aa51a8768336adb1e28dd5d6d297c0507e58cd
dbotti <davide.botti@lastminute.com>
2024-02-02 11:55:43 +0100

    Fix the API

--- webapp/src/main/frontend/logic/AddThingProvider.ts
+++ webapp/src/main/frontend/logic/AddThingProvider.ts
@@ -4,4 +4,4 @@ import {ThingType} from "../Thing";
 export type AddThingProvider = (deviceId: string | null, thingType: ThingType, thingName: string) => Promise<void>;
 
 export const RestAddThingProvider: AddThingProvider = (deviceId: string | null, thingType: ThingType, thingName: string) =>
-    staticRestClient.post(`/v1/things/add/123`, {thingType, thingName})
+    staticRestClient.post(`/v1/things/add`, {deviceId, thingType, thingName})
